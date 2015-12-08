8f07d7280c65a79dc35fae55f754c28e3c90964f
Sean Esteva <seangeleno@gmail.com>
2015-12-08 10:27:38 -0800

    added devDependencies

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp      = require('gulp')
+,   concat    = require('gulp-concat')
+,   uglify    = require('gulp-uglify')
+,   uglifycss = require('gulp-uglifycss')
+,   nodemon   = require('gulp-nodemon')
+
+
+
+gulp.task('default', [])
