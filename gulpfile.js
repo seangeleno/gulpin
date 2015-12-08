6f5c54002939a28e57c8248621c7bfb87132bde8
Sean Esteva <seangeleno@gmail.com>
2015-12-08 10:33:24 -0800

    tested basic gulp commands

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,14 @@ var gulp      = require('gulp')
 ,   uglifycss = require('gulp-uglifycss')
 ,   nodemon   = require('gulp-nodemon')
 
+  // gulp.task('test', function(){
+  //   console.log('Test task is running. All is right in the world');
+  // })
 
+  gulp.task('concat-css', function(){
+    gulp.src('public/dev/css/*.css')
+  })
 
-gulp.task('default', [])
+  gulp.task('default', ["test"], function(){
+    console.log('Default task: winning!');
+  })
